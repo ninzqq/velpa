e19cbc2971eadcbcd253b74de16981e5400fa28f
Janne Niinisalo <janne.niinisalo@gmail.com>
2024-03-22 20:35:25 +0000

    Add OSM map
    All sorts of other thingies as well...

--- velpa/lib/widgets/markertile.dart
+++ velpa/lib/widgets/markertile.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:velpa/local_models/models.dart';
+import 'package:velpa/models/models.dart';
 
 class MarkerListTile extends StatelessWidget {
   final int index;
