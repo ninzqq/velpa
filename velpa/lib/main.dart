6bfc841a583d986852cb1c1fdd202ab4af97b63c
Janne Niinisalo <janne.niinisalo@gmail.com>
2024-06-13 01:08:24 +0300

    Add ensure initialization (needed when built for mobile)

--- velpa/lib/main.dart
+++ velpa/lib/main.dart
@@ -12,6 +12,8 @@ import 'firebase_options.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
