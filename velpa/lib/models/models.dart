8b2dde1efc8951ed3d8d892d1213f717fa64dd59
Janne Niinisalo <janne.niinisalo@gmail.com>
2024-07-15 03:49:17 +0300

    Add mapmarker class

--- velpa/lib/models/models.dart
+++ velpa/lib/models/models.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class LastKnownUserPosition with ChangeNotifier {
   double lat;
@@ -17,7 +18,62 @@ class LastKnownUserPosition with ChangeNotifier {
   }
 }
 
-class UserMarkers with ChangeNotifier {
+class MapMarker {
+  String id;
+  String title;
+  String description;
+  String createdBy;
+  DateTime createdAt;
+  DateTime updatedAt;
+  GeoPoint location;
+  List<String> photos;
+  bool isPublic;
+
+  MapMarker({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.createdBy,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.location,
+    required this.photos,
+    required this.isPublic,
+  });
+
+  // Muuntaa Firestore-dokumentin MapMarker-olioksi
+  factory MapMarker.fromFirestore(DocumentSnapshot doc) {
+    Map data = doc.data() as Map;
+    return MapMarker(
+      id: data['id'] ?? '',
+      title: data['title'] ?? '',
+      description: data['description'] ?? '',
+      createdBy: data['created_by'] ?? '',
+      createdAt: (data['created_at'] as Timestamp).toDate(),
+      updatedAt: (data['updated_at'] as Timestamp).toDate(),
+      location: data['location'] ?? const GeoPoint(0, 0),
+      photos: List<String>.from(data['photos'] ?? []),
+      isPublic: data['public'] ?? true,
+    );
+  }
+
+  // Muuntaa MapMarker-olion Map-muotoon Firestorea varten
+  Map<String, dynamic> toFirestore() {
+    return {
+      'id': id,
+      'title': title,
+      'description': description,
+      'created_by': createdBy,
+      'created_at': createdAt,
+      'updated_at': updatedAt,
+      'location': location,
+      'photos': photos,
+      'public': isPublic,
+    };
+  }
+}
+
+class MapMarkers with ChangeNotifier {
   List<Marker> markers = [];
 
   void add(Marker marker) {
