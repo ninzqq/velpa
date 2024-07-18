8b38d5ad57021436028177365b059a9eb2d383a0
Janne Niinisalo <janne.niinisalo@gmail.com>
2024-07-18 21:27:36 +0300

    add initial drawer

--- velpa/lib/main.dart
+++ velpa/lib/main.dart
@@ -192,8 +192,39 @@ class HomeScreenState extends State<HomeScreen> {
         //body: screens[bottomnavbarindex.idx],
         //bottomNavigationBar: BottomNavBar(
         //  bottomnavbarindex: bottomnavbarindex,
-        body: Center(
-          child: currentUser == null ? buildAuthForm() : buildSignOutButton(),
+        body: const Center(
+          child: OSMMapScreen(),
+        ),
+        drawer: Drawer(
+          // Add a ListView to the drawer. This ensures the user can scroll
+          // through the options in the drawer if there isn't enough vertical
+          // space to fit everything.
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: [
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Text('Drawer Header'),
+              ),
+              ListTile(
+                title: const Text('Item 1'),
+                onTap: () {
+                  // Update the state of the app.
+                  // ...
+                },
+              ),
+              ListTile(
+                title: const Text('Item 2'),
+                onTap: () {
+                  // Update the state of the app.
+                  // ...
+                },
+              ),
+            ],
+          ),
         ),
       );
     } else {
