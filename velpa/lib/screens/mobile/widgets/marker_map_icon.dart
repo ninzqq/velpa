02f114e066d10a8c161415cbc07a23527fd968b7
Janne Niinisalo <janne.niinisalo@gmail.com>
2024-11-22 21:27:07 +0200

    Add focus to tapped marker

--- velpa/lib/screens/mobile/widgets/marker_map_icon.dart
+++ velpa/lib/screens/mobile/widgets/marker_map_icon.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:latlong2/latlong.dart';
 import 'package:logger/logger.dart';
 import 'package:velpa/models/local_models.dart';
+import 'package:velpa/models/models.dart';
 import 'package:velpa/screens/mobile/widgets/marker_details_bottom_sheet.dart';
+import 'package:velpa/utils/snackbar.dart';
 
 class MarkerMapIcon extends ConsumerWidget {
   final String id;
@@ -12,6 +15,8 @@ class MarkerMapIcon extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final appFlags = ref.read(appFlagsProvider);
+    final mapController = ref.read(customMapControllerProvider).mapController;
+    final marker = ref.read(mapMarkersProvider).getMarkerById(id);
     var logger = Logger();
 
     return GestureDetector(
@@ -21,12 +26,23 @@ class MarkerMapIcon extends ConsumerWidget {
       ),
       onTap: () => {
         appFlags.debug ? logger.d('Marker $id tapped!}') : null,
-        showModalBottomSheet(
-          context: context,
-          builder: (BuildContext context) {
-            return MarkerDetailsBottomSheet(id: id);
-          },
-        ),
+        if (marker == null)
+          {
+            showSnackBar('Marker $id not found', const Icon(Icons.error)),
+          }
+        else
+          {
+            if (mapController.camera.zoom < 10)
+              mapController.move(
+                  LatLng(marker.point.latitude - 0.09, marker.point.longitude),
+                  10),
+            showModalBottomSheet(
+              context: context,
+              builder: (BuildContext context) {
+                return MarkerDetailsBottomSheet(id: id);
+              },
+            ),
+          }
       },
     );
   }
